99941063df6381934dca1bc80313831c9a83777f
FabioGMarascia <fabiogaspare.marascia@gmail.com>
2024-05-15 15:57:16 +0200

    M-1 email list

--- js/script.js
+++ js/script.js
@@ -3,7 +3,28 @@ const { createApp } = Vue;
 createApp({
 	data() {
 		return {
-			message: "Hello Vue!",
+			url: `https://flynn.boolean.careers/exercises/api/`,
+			dataTypeUrl: `random/mail`,
+			mailContainer: [],
+			x: null,
 		};
+	},
+	methods: {
+		generateEmail() {
+			axios.get(this.url + this.dataTypeUrl).then((result) => {
+				this.x = result.data.response;
+				this.mailContainer.push(this.x);
+			});
+		},
+		getEmailList(n) {
+			this.generateEmail();
+			for (let i = 0; i < n; i++) {
+				this.generateEmail();
+			}
+		},
+	},
+	mounted() {
+		this.getEmailList(9);
+		console.log(this.mailContainer);
 	},
 }).mount("#app");
